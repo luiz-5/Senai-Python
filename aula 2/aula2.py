76f310d6b472ec7818e3a90528d16455c0c4235c
luiz-5 <luizfelipedallaqua@gmail.com>
2024-12-02 10:00:41 -0300

    na aula de hoje aprendi o que é uma variavel

---
+++ aula 2/aula2.py
@@ -0,0 +1,11 @@
+print('bom dia')
+nome_usuario = 'dallaqua' # tipo -> str =string
+idade_usuario = 21 # tipo -> int = integer
+altura_usuario = 1.77 # tipo -> float
+maior_de_idade = True # tipo -> bool = boolean
+
+print('seja bem-vindo', nome_usuario, 'sua idade é', idade_usuario, 'sua altura é',altura_usuario, 'você é maior de idade',maior_de_idade)
+
+
+
+
